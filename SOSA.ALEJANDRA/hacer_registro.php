7b95169bb79a7ee338fa75daa4422f5842882936
AlejandraLujanSosa <54086013+AlejandraLujanSosa@users.noreply.github.com>
2019-09-25 20:41:04 -0300

    no me dirige a la pagina ok.php
    me tira error en hacer_registro en linea 3 y linea 4.cambie las variables de hacer_ingreso y hacer_login. Agregue el if en listado

---
+++ SOSA.ALEJANDRA/hacer_registro.php
@@ -0,0 +1,10 @@
+<?php
+$miobjeto=new stdClass();
+$miobjeto->Dominio =$_GET['Dominio'];
+$miobjeto->Hora =$_GET['Hora'];
+
+$archivo=fopen('usuarios.txt','a');
+fwrite($archivo, json_encode($miobjeto)."\n");
+fclose($archivo);
+
+?>
