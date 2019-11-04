b2052d56e824396acfa5e53c3d6c98c48fe8b55f
AlejandraLujanSosa <54086013+AlejandraLujanSosa@users.noreply.github.com>
2019-11-03 23:50:25 -0300

    estacionamiento

---
+++ SOSA_TP/HacerLogin.php
@@ -0,0 +1,33 @@
+<?php
+
+$archivo=fopen('RegistroUsuarios.txt','r');
+
+while(!feof($archivo)) 
+{
+  $json = json_decode(fgets($archivo));
+
+  if ($json->Usuario == $_GET['Usuario'])    
+  {
+      if ($json->Clave == $_GET['Clave'])
+      {
+            header("Location: LoginOk.php?Login=$json->Usuario");
+            fclose($archivo);
+            exit();
+      } 
+      else
+      {
+          header("Location: LoginNoOK.php");
+          fclose($archivo);     
+          exit();
+      }
+        
+  }  
+  
+}  
+
+
+ header("Location: UsuarioInexistente.php");
+ fclose($archivo);
+    exit();
+
+?>
